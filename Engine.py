e2e262c8d46f71b4dc502a81c8e50a3e9c8b394f
TristanStoutenburg <tstouten@asu.edu>
2016-12-15 14:47:13 -0700

    Modified the engine so that it uses the screen and painter to do updates, the engine essentially just schedules these updates

--- Engine.py
+++ Engine.py
@@ -1,36 +1,23 @@
 import pygame
 import time
+from Painter import Painter
 
 
 class Engine:
-    
-    DEFAULT_SIZE = (640, 480)
-    DEFAULT_FPS = 30
-    background_color = (255, 255, 255)
 
-    def __init__(self, size = DEFAULT_SIZE, fps = DEFAULT_FPS):
-        self.size = size
+    DEFAULT_FPS = 30
+    fps = DEFAULT_FPS
+    START_TIME = 0.0
+    playtime = None
+
+    def __init__(self, screen, painter, fps = DEFAULT_FPS):
+        self.screen = screen
+        self.painter = painter
         self.fps = fps
         self.clock = pygame.time.Clock()
-        self.playtime = 0.0
-
-    def set_up(self):
-        pygame.init()
-        self.set_up_screen()
-        self.set_up_background()
-
-    def set_up_screen(self):
-        self.screen = pygame.display.set_mode(self.size)
-
-    def set_up_background(self):
-        self.background = pygame.Surface(self.screen.get_size())
-        self.background.fill(self.background_color)
-        self.background = self.background.convert()
-        origin = (0, 0)
-        self.screen.blit(self.background, origin)
+        self.playtime = self.START_TIME
 
     def run(self):
-        self.set_up()
         self.playing = True
         while self.playing:
             self.update()
@@ -38,32 +25,23 @@ class Engine:
 
     def update(self):
         self.update_time()
-        self.update_caption()
-        self.update_display()
+        time = self.playtime
+        caption = self.painter.generate_caption(time)
+        surfaces_with_positions = self.painter.generate_surfaces_with_positions(time)
+        self.screen.update(surfaces_with_positions, caption)
         self.update_playing()
 
     def update_time(self):
         miliseconds = self.clock.tick(self.fps)
         self.playtime += miliseconds / 1000.0
 
-    def update_caption(self):
-        self.caption = "FPS: {0:.2f} Playtime: {1: .2f}".format(self.clock.get_fps(), self.playtime)
-        pygame.display.set_caption(self.caption)
-
-    def update_display(self):
-        pygame.display.flip()
-
     def update_playing(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.playing = False
-            elif event.type == pygame.KEYDOWN and event.type == pygame.K_ESCAPE:
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 self.playing = False
 
 
     def stop(self):
-        print("This game was played for {0:.2f} seconds".format(self.playtime))
-        pygame.display.quit()
-
-g = Engine()
-g.run()
+        self.screen.quit()
